673f0b78e8d24abcbc7486aff5dbb6a5dd84923b
Elvis Felix <elvisjaspion@gmal.com>
2021-05-23 17:29:40 -0300

    Desafio finalizado

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,4 @@
+  
 import React, { useState } from 'react';
 
 import { Header } from '../components/Header';
@@ -11,18 +12,38 @@ interface Task {
 }
 
 export function Home() {
-  // const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleAddTask(newTaskTitle: string) {
-    //TODO - add new task if it's not empty
+
+    if(!newTaskTitle) return;
+
+    const newTask =  {
+      id: new Date().getTime(),
+      title: newTaskTitle,
+      done: false,
+    };
+
+    setTasks([...tasks, newTask]);
   }
 
   function handleMarkTaskAsDone(id: number) {
-    //TODO - mark task as done if exists
+    const newTasks = tasks.map(task =>
+        task.id === id
+        ? {
+          ...task,
+          done: !task.done
+        }
+        : task
+      );
+
+      setTasks(newTasks);
   }
 
   function handleRemoveTask(id: number) {
-    //TODO - remove task from state
+    const newTasks = tasks.filter(task => task.id !== id);
+
+    setTasks(newTasks);
   }
 
   return (
